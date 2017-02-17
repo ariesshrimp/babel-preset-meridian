f0368d7778dc8b4aaa5f222a96da999f16c32fa0
joefraley <jose.fraley@gmail.com>
2017-02-17 12:26:38 -0800

    Include webpack lazy-load plugin.

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ module.exports = {
     require('babel-plugin-transform-decorators-legacy'),
 
     /**
+     * @see https://babeljs.io/docs/plugins/transform-class-properties/
      * makes this possible:
      * class Button extends React.Component {
      *  static propTypes = {...}
@@ -28,10 +29,20 @@ module.exports = {
     require('babel-plugin-transform-class-properties'),
 
     /**
+     * @see https://babeljs.io/docs/plugins/transform-object-rest-spread/
      * makes this possible:
      * const props = {a:1, b:2, c:3}
      * <Button {...props} /> // Button.props === {a:1, b:2, c:3}
      */
     require('babel-plugin-transform-object-rest-spread'),
+
+    /**
+     * @see https://webpack.js.org/guides/code-splitting-import/#dynamic-import
+     * makes webpacks lazy-load syntax possible
+     *  e.g. import('react').then(({ Component }) => {
+     *   class Button extends Component {...}
+     * })
+     */
+    require('babel-plugin-syntax-dynamic-import'),
   ]
 }
